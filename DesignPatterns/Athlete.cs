967bd9aa094acfa746d2aa5eb03e7e452af856c1
ThePriestt <49431616+ThePriestt@users.noreply.github.com>
2021-03-30 16:25:41 +0300

    Add project files.

---
+++ DesignPatterns/Athlete.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns
+{
+    class Athlete
+    {
+        public void Up() 
+        {
+            Console.WriteLine("I am getting up");
+        }
+
+        public void Down()
+        {
+            Console.WriteLine("I am getting down");
+        }
+    }
+}
---
+++ DesignPatterns/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DesignPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Athlete athlete = new Athlete();
+            CommandUp commandUp = new CommandUp(athlete);
+            CommandDown commandDown = new CommandDown(athlete);
+            Trainer trainer = new Trainer(commandUp);
+            trainer.Train();
+            trainer.Command = commandDown;
+            trainer.Train();
+        }
+    }
+}
